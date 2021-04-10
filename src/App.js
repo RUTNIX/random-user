eb535867c88114bf911b042f2d1098d092fb9a3b
Али Бейшенбеков <alibejsenbekov@MacBook-Air-Ali.local>
2021-04-10 16:09:13 +0600

    randomUser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import  './App.css'
+import {API} from './src'
 
-function App() {
+export default function App() {
+  const [val, setVal]=useState('')
+  const [data, setData] =useState([])
+
+  const getSearchUserlenght=async(event)=>{
+    event.preventDefault()
+    let url = API + val
+
+    let req = await fetch(url)
+    let resp = await req.json()
+    setData(resp.results)
+    setVal('')
+    console.log(resp.results)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <form onSubmit={getSearchUserlenght}>
+        <input
+          
+          className='input'
+          placeholder=' Введите число'
+          value={val}
+          onChange={e=>setVal(e.target.value)}
+        />
+ 
+        <button>search</button>
+
+      </form>
+
+      <div className='cards'>
+              {data && data.length!==0 ?
+                data.map((el, index)=>{
+                  return(
+                    <div className='card' key={index}>
+                      <img className="img" src={el.picture.large} alt='img'/>
+                      <p className='gender'> gender: {el.gender}</p>
+                      <p className='name' > name: {el.name.title + el.name.first}</p>
+                      <p className='age'>age: {el.dob.age}</p>
+
+                    </div>
+                  )  
+                })
+                : <h3> Пока пусто</h3>
+              }
+      </div>
+      
+
     </div>
-  );
+  )
 }
 
-export default App;
